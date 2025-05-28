936ff439b81b2930e8b47959d3abe4a392a35f04
DziqG2NK <mracin.walendzik.dysk@gmail.com>
2025-05-28 17:15:58 +0200

    Styles for quiz

--- frontend/src/components/quiz/QuizQuestionNew.js
+++ frontend/src/components/quiz/QuizQuestionNew.js
@@ -14,74 +14,76 @@ const QuizQuestion = ({
     const checked = e.target.checked;
 
     setUserAnswers((prev) => {
-      const prevAnswers = prev[questionIndex] || [];
+      const prevAnswers = prev[questionIndex] || []
 
       if (checked) {
-        // Dodaj tylko, jeśli jeszcze nie ma
         if (!prevAnswers.includes(answerIndex)) {
           return {
             ...prev,
             [questionIndex]: [...prevAnswers, answerIndex],
-          };
+          }
         }
         return prev;
-      } else {
-        // Usuń zaznaczoną odpowiedź
+      } 
+
+      else {
         return {
           ...prev,
           [questionIndex]: prevAnswers.filter((i) => i !== answerIndex),
-        };
+        }
       }
-    });
-  };
+    })
+  }
 
   return (
     <>
-      <div className="question-box">
-        <h2>
-          {questionNumber}. {question}
-        </h2>
-      </div>
+    <div className="quiz-question-main-view">
+        <div className="question-box">
+            <h2>
+            {questionNumber}. {question}
+            </h2>
+        </div>
 
-      <div className="options-box">
-        {options.map((option, index) => (
-          <div className="quiz-option" key={index}>
-            <input
-              type={type}
-              className={`${type}-option`}
-              onChange={handleCheckboxChange(index)}
-              checked={userAnswers[questionIndex]?.includes(index) || false}
-            />
-            <p>{option}</p>
-          </div>
-        ))}
-      </div>
+        <div className="options-box">
+            {options.map((option, index) => (
+            <div className="quiz-option" key={index}>
+                <input
+                type={type}
+                className={`${type}-option`}
+                onChange={handleCheckboxChange(index)}
+                checked={userAnswers[questionIndex]?.includes(index) || false}
+                />
+                <p>{option}</p>
+            </div>
+            ))}
+        </div>
 
-      <div className="navigation-box">
-        {questionNumber > 1 && (
-          <div
-            className="quiz-back-button"
-            onClick={() => setSelectedQuestion(questionNumber - 2)}
-          >
-            Wstecz
-          </div>
-        )}
-        {questionNumber === allQuestionsNumber ? (
-          <div
-            className="quiz-finish-button"
-            onClick={() => setSelectedQuestion(questionNumber)}
-          >
-            Zakończ
-          </div>
-        ) : (
-          <div
-            className="quiz-finish-button"
-            onClick={() => setSelectedQuestion(questionNumber)}
-          >
-            Dalej
-          </div>
-        )}
-      </div>
+        <div className="navigation-box">
+            {questionNumber > 1 && (
+            <div
+                className="quiz-back-button"
+                onClick={() => setSelectedQuestion(questionNumber - 2)}
+            >
+                Wstecz
+            </div>
+            )}
+            {questionNumber === allQuestionsNumber ? (
+            <div
+                className="quiz-finish-button"
+                onClick={() => setSelectedQuestion(questionNumber)}
+            >
+                Zakończ
+            </div>
+            ) : (
+            <div
+                className="quiz-finish-button"
+                onClick={() => setSelectedQuestion(questionNumber)}
+            >
+                Dalej
+            </div>
+            )}
+        </div>
+    </div>
     </>
   );
 };
