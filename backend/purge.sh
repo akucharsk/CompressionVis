d7af9e0ad34067db0939a9142eb6e8254b317b23
Antoni Kucharski <antek.kuchar@gmail.com>
2025-05-29 07:55:16 +0200

    demo updates

--- backend/purge.sh
+++ backend/purge.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
+RESERVED_FILENAMES="('example1.mp4', 'example2.mp4', 'example3.mp4', 'example4.mp4', 'deadpool.mp4')"
+RESERVED_DIRNAMES=('example1' 'example2' 'example3' 'example4' 'deadpool')
+for dirname in "${RESERVED_DIRNAMES[@]}"; do
+  cp -rt "static/temp/" "static/frames/$dirname"
+done
+
 rm -rf static/frames/*
 rm -rf static/compressed_videos/*
 
+for dirname in "${RESERVED_DIRNAMES[@]}"; do
+  cp -rt "static/temp/$dirname" "static/frames/"
+  rm -rf "static/temp/$dirname"
+done
+
 DB="db.sqlite3"
 VIDEO_TABLE="compressor_video"
-
-RESERVED_FILENAMES="('example1.mp4', 'example2.mp4', 'example3.mp4', 'example4.mp4', 'deadpool.mp4')"
 
 sqlite3 "$DB" "DELETE FROM $VIDEO_TABLE WHERE filename NOT IN $RESERVED_FILENAMES"
--- frontend/src/components/videoPreview/OptionsSelection.js
+++ frontend/src/components/videoPreview/OptionsSelection.js
@@ -18,9 +18,9 @@ const OptionsSection = ({ handleCompress }) => {
             value: parameters.bandwidth,
             onChange: updateParam("bandwidth"),
             options: [
-                { value: "64k", label: "64kB/s" },
-                { value: "128k", label: "128kB/s" },
-                { value: "1M", label: "1MB/s" },
+                { value: "64k", label: "64kb/s" },
+                { value: "128k", label: "128kb/s" },
+                { value: "1M", label: "1Mb/s" },
             ],
         },
         {
@@ -30,7 +30,10 @@ const OptionsSection = ({ handleCompress }) => {
             options: [
                 { value: "1920x1080", label: "1920x1080" },
                 { value: "1280x720", label: "1280x720" },
-                { value: "400x400", label: "400x400" },
+                { value: "960x540", label: "960x540" },
+                { value: "854x480", label: "854x480" },
+                { value: "640x360", label: "640x360" },
+                { value: "426x240", label: "426x240" },
             ],
         },
         {
@@ -50,7 +53,10 @@ const OptionsSection = ({ handleCompress }) => {
             options: [
                 { value: "10", label: "10" },
                 { value: "20", label: "20" },
+                { value: "25", label: "25" },
                 { value: "35", label: "35" },
+                { value: "35", label: "35" },
+                { value: "40", label: "40" },
                 { value: "51", label: "51 (max)" },
             ],
         },
