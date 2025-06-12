357f96844adac6cd707a6cb1d85b2a9f90e9a9b1
Antoni Kucharski <antek.kuchar@gmail.com>
2025-06-12 08:10:23 +0200

    feat: improved startup.py

--- backend/startup.py
+++ backend/startup.py
@@ -1,7 +1,18 @@
 from compressor import models  # zmień 'yourapp' na nazwę swojej aplikacji Django
+import os
+import shutil
 
 models.Video.objects.all().delete()
+reserved_filenames = os.listdir(os.path.join("static", "videos"))
+reserved_frame_dirs = set(map(lambda x: os.path.splitext(x)[0], reserved_filenames))
 
-for i in range(1, 5):
-    models.Video.objects.create(filename=f"example{i}.mp4", width=1920, height=1080)
+for file in os.listdir(os.path.join("static", "compressed_videos")):
+    os.remove(os.path.join("static", "compressed_videos", file))
+for file in os.listdir(os.path.join("static", "frames")):
+    if file not in reserved_frame_dirs:
+        shutil.rmtree(os.path.join("static", "frames", file))
+
+for vid in reserved_filenames:
+    if vid.endswith(".mp4"):
+        models.Video.objects.create(filename=vid, width=1920, height=1080)
 exit()
