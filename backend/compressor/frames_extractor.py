39cd5a9e3fe97665d1679ba59c52b0915a5f0602
Antoni Kucharski <akucharski@qualtrics.com>
2025-10-20 23:01:43 +0200

    refactor: improved frames extraction, refactored endpoints to extract frames and metrics after calling /compress

---
+++ backend/compressor/frames_extractor.py
@@ -0,0 +1,82 @@
+import os
+import subprocess
+import threading
+import json
+import sys
+
+from django.db import IntegrityError
+
+from compression_vis import settings
+from compressor import models
+from . import serializers
+from django.contrib.staticfiles import finders
+
+class FramesExtractor:
+    def __init__(self, video: models.Video):
+        self.video = video
+        self.video_name = os.path.splitext(video.filename)[0]
+        self.frames_dir = os.path.join(settings.BASE_DIR, "static", "frames", self.video_name)
+        self.video_path = finders.find(os.path.join("compressed_videos", video.filename))
+
+    def _get_frames_info(self):
+        result = subprocess.run([
+            "ffprobe",
+            "-show_frames",
+            "-select_streams", "v",
+            "-print_format", "json",
+            self.video_path
+        ], capture_output=True, text=True)
+
+        data = json.loads(result.stdout)
+
+        frames = []
+        i = 0
+        frames_dir = self.video.filename.split(".")[0]
+
+        for frame in data.get("frames", []):
+            pict_type = frame.get("pict_type")
+            if pict_type in ["I", "P", "B"]:
+                frames.append({
+                    "frame_number": i,
+                    "type": pict_type,
+                    "pts_time": frame.get("pts_time"),
+                    "video_id": self.video.id,
+                    "image_url": f"frames/{frames_dir}/frame_{i}.png",
+                    "pkt_size": frame.get("pkt_size"),
+                })
+                i += 1
+
+        frames_serializer = serializers.CreateFramesSerializer(data={ "frames": frames })
+        frames_serializer.is_valid(raise_exception=True)
+        try:
+            frames_serializer.save()
+        except IntegrityError:
+            return
+
+    def _extract_and_update_status(self):
+        try:
+            self._get_frames_info()
+            os.makedirs(self.frames_dir, exist_ok=True)
+            process = subprocess.Popen([
+                "ffmpeg", "-i", self.video_path,
+                "-frame_pts", "true",
+                f"{self.frames_dir}/frame_%d.png"
+            ], stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+            process.wait()
+
+            self.video.frames_extraction_completed = True
+        except Exception as e:
+            print(f"Error during frame extraction: {e}")
+        finally:
+            print("Extraction completed")
+            sys.stdout.flush()
+            self.video.frames_extraction_in_progress = False
+            self.video.save()
+            print(models.Video.objects.get(id=self.video.id).frames_extraction_in_progress)
+            sys.stdout.flush()
+
+    def start_extraction_job(self):
+        self.video.frames_extraction_in_progress = True
+        self.video.save()
+        thread = threading.Thread(target=self._extract_and_update_status, daemon=True)
+        thread.start()
