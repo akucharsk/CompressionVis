1c14861e8391e86c02be7660328bf24c2eb9cdbc
Apiotr16st <piotrandres@student.agh.edu.pl>
2025-11-02 21:28:58 +0100

    Fixed cache

--- frontend/src/utils/queryClient.js
+++ frontend/src/utils/queryClient.js
@@ -1,3 +1,11 @@
 import { QueryClient } from "@tanstack/react-query";
 
+export const VIDEO_MAP_KEY = ["videoMap"];
+
+const saved = localStorage.getItem("videoMap");
+
 export const queryClient = new QueryClient();
+
+if (saved) {
+    queryClient.setQueryData(VIDEO_MAP_KEY, JSON.parse(saved));
+}
